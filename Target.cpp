21437eac23715631d6999920026a7f4277906c66
Jimmy Hong <su010331@naver.com>
2019-05-12 13:12:48 +0900

    update

--- Target.cpp
+++ Target.cpp
@@ -1,4 +1,5 @@
-
+// Data_structure.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
 
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
@@ -6,50 +7,74 @@
 #include<iostream>
 #include<vector>
 
+using namespace std;
 
-int **make2array(int col, int row) {
+int recursiveminus(int sum, vector<int> Q, int target);
+int recursiveplus(int sum, vector<int> Q, int target);
 
-	int **arr;
+int recursiveminus(int sum, vector<int> Q, int target) {
+	sum -= Q.back();
 
-	arr = (int**)calloc(row, sizeof(int *));
-
-
-	for (int j = 0; j < row; j++) {
-		arr[j] = (int*)calloc(col, sizeof(int));
+	if (Q.size() > 1) {
+		Q.pop_back();
+		return recursiveplus(sum, Q, target) + recursiveminus(sum, Q, target);
 	}
+	else if (Q.size() == 1 && sum == target) {
+		return 1;
+	}
+	else if (Q.size() == 1 && sum != target) {
 
-	return arr;
+		return 0;
+	}
 }
 
 
+int recursiveplus(int sum, vector<int> Q, int target) {
+	sum += Q.back();
+	if (Q.size() > 1) {
+		Q.pop_back();
+		return recursiveplus(sum, Q, target) + recursiveminus(sum, Q, target);
+	}
+	else if (Q.size() == 1 && sum == target) {
+		return 1;
+	}
+	else if (Q.size() == 1 && sum != target) {
+		return 0;
+	}
 
+}
 
-int main() {
+int solution(vector<int>numbers, int  target) {
+	int answer = recursiveplus(0, numbers, target) + recursiveminus(0, numbers, target);
+	return answer;
+}
 
-	int col, row;
+int main()
+{
+	vector<int> Q;
+	int number, target;
 
-	int **arr;
-	
-	scanf("%d %d", &col, &row);
+	scanf("%d", &number);
 
-	arr = make2array(col, row);
-	
+	scanf("%d", &target);
 
-	for (int i = 0; i < row; i++) {
-		for (int j = 0; j < col; j++) {
-			scanf("%d", &arr[i][j]);
-		}
+	int *arr;
+
+	arr = (int*)calloc(sizeof(int), number);
+
+	for (int i = 0; i < number; i++)
+	{
+		scanf("%d", &arr[i]);
+		Q.push_back(arr[i]);
 	}
 
 
-	for (int i = 0; i < row; i++) {
-		for (int j = 0; j < col; j++) {
-			std::printf("%3d", arr[i][j]);
-		}
-		std::printf("\n");
-	}
-	std::printf("\n");
+	int answer;
 
+	answer = solution(Q, target);
+
+
+	printf("%d\n", answer);
 
 	return 0;
 }
